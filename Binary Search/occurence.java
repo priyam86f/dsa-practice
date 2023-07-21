aac4d74f9b5628635fb8d39c17c49789b0cb85d5
priyam77 <priyammehta616@gmail.com>
2023-07-21 18:24:23 +0530

    added printArray method, to print the final result and fixed a bug

--- Binary Search/occurence.java
+++ Binary Search/occurence.java
@@ -1,10 +1,12 @@
 public class occurence {
     public static void main(String[] args) {
         int[] nums = {2,4,6,8,8,8,10,12,14};
+        int[] res = occ(nums, 8);
+        printArray(res);
     }
 
     public static int[] occ(int[] nums,int target){
-        //find the first and last occurence
+        
         int[] arr = new int[2];
         arr[0]=FIRST_OCCURENCE(nums, target);
         arr[1]=LAST_OCCURENCE(nums, target);
@@ -27,10 +29,10 @@ public class occurence {
                 high=mid-1;
             }
 
-            if(target>=nums[mid]){
+            if(target>nums[mid]){
               low=mid+1;
             }
-            else if(target<=nums[mid]){
+            else if(target<nums[mid]){
                 high=mid-1;
                
             }
@@ -54,14 +56,20 @@ public class occurence {
                 low=mid+1;
             }
 
-            if(target>=nums[mid]){
+            if(target>nums[mid]){
               low=mid+1;
             }
-            else if(target<=nums[mid]){
+            else if(target<nums[mid]){
                 high=mid-1;
                
             }
         }
         return res;
+    }
+
+    public static void printArray(int[] nums){
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]+" ");
+        }
     }
 }
