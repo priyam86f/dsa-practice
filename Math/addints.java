5e4c2215fb067ff830ffbada415e3a9298f4fc8a
priyam77 <priyammehta616@gmail.com>
2023-07-24 16:34:57 +0530

    math/add two integers

---
+++ Math/addints.java
@@ -0,0 +1,10 @@
+public class addints {
+    public static void main(String[] args) {
+        int x = addintegers(10, -5);
+        System.out.println(x);
+    }
+
+    public static int addintegers(int num1,int num2){
+        return num1+num2;
+    }
+}
