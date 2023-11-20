a92b7d503e0d84c65c9b55e5da8e184d92bcdf2c
priyam86f <136905851+priyam86f@users.noreply.github.com>
2023-11-20 16:52:54 +0530

    Linked List ListNode

---
+++ Linked List/Node.java
@@ -0,0 +1,5 @@
+public class Node{
+    int data;
+    Node next; // because we need a variable of the type of object we are referring to.
+    //in this case, we are referring to the next node..
+}
