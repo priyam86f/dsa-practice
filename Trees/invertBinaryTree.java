b8041a93ce400d623396a58886d8d3c0d7d423f3
priyam123 <priyamatwork24@gmail.com>
2024-01-19 21:57:48 +0530

    226.Invert Binary Tree

---
+++ Trees/invertBinaryTree.java
@@ -0,0 +1,18 @@
+
+
+public class invertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left=root.right;
+        root.right=tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
