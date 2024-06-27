58edf4f67acbb7a6a35f08d0669b8cab6e704a62
priyam123 <priyamatwork24@gmail.com>
2024-06-27 18:58:20 +0530

    3152. Special Array II

---
+++ Arrays/specialarrayII.java
@@ -0,0 +1,31 @@
+public class specialarrayII {
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 1, 2, 6 };
+        int[][] queries = { { 0, 4 } };
+        boolean[] res = isArraySpecial(nums, queries);
+        System.out.println(res);
+    }
+
+    public static boolean[] isArraySpecial(int[] nums, int[][] queries) {
+
+        int n = nums.length;
+
+        int[] d = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] % 2 != nums[i - 1] % 2) {
+                d[i] = d[i - 1];
+            } else {
+                d[i] = i;
+            }
+        }
+
+        int m = queries.length;
+        boolean[] ans = new boolean[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = d[queries[i][1]] <= queries[i][0];
+        }
+       
+        return ans;
+    }
+}
